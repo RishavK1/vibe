f2ab474d64db050d7903d79126215b6ac951aad4
RishavK1 git config --global user.email <rishavkamboj75@gmail.com>
2025-10-26 14:43:21 +0530

    03-trpc-setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,20 @@
+import { Suspense } from "react";
+import { getQueryClient, trpc } from "@/trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { Client } from "./client";
 
-const Page =  () =>{
+const Page = async () => {
 
-  return(
-    <div>
-      <h1>Hello World</h1>
-    </div>
+  const queryClient = await getQueryClient();
+
+  void queryClient.prefetchQuery(trpc.hello.queryOptions({ text: 'RK' }));
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Suspense fallback={<p>Loading...</p>}>    
+        <Client />
+      </Suspense>
+    </HydrationBoundary>
+
   )
 }
 
