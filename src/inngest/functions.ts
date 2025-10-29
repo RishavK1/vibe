46853177de8c6320e42e12789035a75c383880c0
RishavK1 git config --global user.email <rishavkamboj75@gmail.com>
2025-10-29 14:21:04 +0530

    ai-jobs

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,23 @@
+import { gemini, createAgent } from "@inngest/agent-kit";
+
+
 import { inngest } from "./client";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event, step }) => {
-    await step.sleep("wait-a-moment", "1s");
-    return { message: `Hello ${event.data.email}!` };
+  async ({ event }) => {
+    const codeAgent = createAgent({
+      name: "code-agent",
+      system: "You are an expert nextjs developer.  You write readbale maintainable code. You write simple nextjs and Reactjs snippets.",
+      model: gemini({ model: "gemini-2.0-flash" }),
+    });
+
+    const { output } = await codeAgent.run(
+      `Write the following snippets: ${event.data.email}`,
+    );
+    console.log(output);
+
+    return { output };
   },
 );
