62e6c7d1430154f9082fc0fc11b7350b0905e8f6
RishavK1 git config --global user.email <rishavkamboj75@gmail.com>
2025-11-01 14:14:13 +0530

    projects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 'use client'
-import { useMutation, useQuery } from "@tanstack/react-query";
+import { useRouter } from "next/navigation";
+
+import { useMutation } from "@tanstack/react-query";
 
 import { toast } from "sonner";
 
@@ -10,22 +12,27 @@ import { useState } from "react";
 
 const Page = () => {
 
+  const router = useRouter();
   const [value, setValue] = useState('');
 
   const trpc = useTRPC();
-  const {data : messages} = useQuery(trpc.messages.getMany.queryOptions())
 
-  const createMessage = useMutation(trpc.messages.create.mutationOptions({
-    onSuccess: () => {
-      toast.success('Message created successfully');
+  const createProject = useMutation(trpc.projects.create.mutationOptions({
+    onError: (error) => {
+      toast.error(error.message);
+    },
+    onSuccess : (data) =>{
+
+      router.push(`/projects/${data.id}`);
     }
   }));
   return (
 
-    <div className="p-4 max-w-7xl mx-auto">
+    <div className="h-screen w-screen flex  items-center justify-center">
+      <div className="max-w-7xl mx-auto flex items-center flex-col gap-y-4 justify-center">
       <Input value={value} onChange={(e) => setValue(e.target.value)} />
-      <Button disabled={createMessage.isPending} onClick={() => createMessage.mutate({ value: value })}>Bg Job Invocation</Button>
-      {JSON.stringify(messages, null, 2)}
+      <Button disabled={createProject.isPending} onClick={() => createProject.mutate({ value: value })}>Submit</Button>
+      </div>
     </div>
   )
 }
